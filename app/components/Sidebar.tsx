f0f3cb2163b9f3a1295e70bcd170ba44748601d0
Rafael Montero <developer@rmontero.me>
2026-09-03 11:10:07 -0600

    Add API call from SSR page

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from "react-icons/io5";
+import {
+  IoBrowsersOutline,
+  IoCalculator,
+  IoFootball,
+  IoLogoReact,
+} from "react-icons/io5";
 import { SidebarMenuItem } from "./SidebarMenuItem";
 
 const menuItems = [
@@ -14,6 +19,12 @@ const menuItems = [
     icon: <IoCalculator size={40} />,
     title: "Counter",
     subTitle: "Client Side Counter",
+  },
+  {
+    path: "/dashboard/pokemons",
+    icon: <IoFootball size={40} />,
+    title: "Pokemons",
+    subTitle: "Static Generation",
   },
 ];
 
---
+++ app/dashboard/pokemons/page.tsx
@@ -0,0 +1,18 @@
+const getPokemons = async (limit = 20, offset = 0) => {
+  const data = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`,
+  ).then((res) => res.json());
+
+  return data;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons();
+
+  return (
+    <>
+      <span>Pokemon List</span>
+      <div>{JSON.stringify(pokemons)}</div>
+    </>
+  );
+}
