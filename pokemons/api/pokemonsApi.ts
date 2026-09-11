2f2096c5bdb47e83c032453f51ce5a2012a221d7
Rafael Montero <developer@rmontero.me>
2026-09-10 20:23:10 -0600

    Revert "Add Axios-powered PokeAPI client"
    This reverts commit cabaa6a27bf14d955c491e5362e02b9483f404f5.

--- pokemons/api/pokemonsApi.ts
+++ pokemons/api/pokemonsApi.ts
@@ -1,42 +1,35 @@
 import { notFound } from "next/navigation";
-import axios from "axios";
 import { PokemonsResponse, SimplePokemon, Pokemon } from "@/pokemons";
-
-export const pokemonApi = axios.create({
-  baseURL: `https://pokeapi.co/api/v2`,
-});
 
 export const getPokemons = async (
   limit = 20,
   offset = 0,
 ): Promise<SimplePokemon[]> => {
-  const { data: pokemonsData }: { data: PokemonsResponse } =
-    await pokemonApi.get(`/pokemon?limit=${limit}&offset=${offset}`);
+  const data: PokemonsResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`,
+  ).then((res) => res.json());
 
-  const pokemons = pokemonsData.results.map((pokemon) => ({
+  const pokemons = data.results.map((pokemon) => ({
     id: pokemon.url.split("/").at(-2)!,
     name: pokemon.name,
   }));
+
+  // throw new Error("Error desconocido. ");
 
   return pokemons;
 };
 
 export const getPokemon = async (name: string): Promise<Pokemon> => {
   try {
-    // const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
-    //   // cache: "force-cache",
-    //   next: {
-    //     revalidate: 60 * 60 * 30 * 6,
-    //   },
-    // }).then((resp) => resp.json());
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
+      // cache: "force-cache",
+      next: {
+        revalidate: 60 * 60 * 30 * 6,
+      },
+    }).then((resp) => resp.json());
 
-    const { data: pokemonData }: { data: Pokemon } = await pokemonApi.get(
-      `/pokemon/${name}`,
-    );
-
-    return pokemonData;
+    return pokemon;
   } catch (error) {
-    console.log(error);
     notFound();
   }
 };
