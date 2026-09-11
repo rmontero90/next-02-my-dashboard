8dd8bbca9bd40b2bbffd828b827c4c018b6e858e
Rafael Montero <developer@rmontero.me>
2026-09-10 20:58:51 -0600

    Make the sidebar responsive

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -15,20 +15,21 @@ export const Sidebar = () => {
   return (
     <div
       id="menu"
-      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-[72px] md:w-[400px] shrink-0 left-0 overflow-y-auto"
+      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-[72px] md:w-[220px] shrink-0 left-0 overflow-y-auto"
     >
       <div id="logo" className="my-4 px-2 md:px-6">
         <h1 className="flex items-center justify-center md:justify-start text-lg md:text-2xl font-bold text-white">
           <IoLogoReact className="m-2" />
-          <span className="hidden md:inline">Dash</span>
+          <span className="hidden md:inline">PokeAPI</span>
         </h1>
-        <p className="hidden md:block text-slate-500 text-sm">
-          Manage your actions and activities
-        </p>
+        <p className="hidden md:block text-slate-500 text-sm">Static pages</p>
       </div>
       <div id="profile" className="px-2 md:px-6 py-6 md:py-10">
         <p className="hidden md:block text-slate-500">Welcome back,</p>
-        <a href="#" className="flex justify-center md:justify-start items-center md:space-x-2">
+        <a
+          href="#"
+          className="flex justify-center md:justify-start items-center md:space-x-2"
+        >
           <span>
             <Image
               className="rounded-full w-8 h-8"
